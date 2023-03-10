2d8b21b9f47872e8bf36e5b10bfbe1ff81f7df71
Anukach14 <anukacherikar@gmail.com>
2023-03-10 19:06:54 +0530

    modified:   Dentist/Dentist/__pycache__/settings.cpython-311.pyc 	modified:   Dentist/Dentist/settings.py 	modified:   Dentist/app/__pycache__/admin.cpython-311.pyc 	modified:   Dentist/app/__pycache__/models.cpython-311.pyc 	modified:   Dentist/app/__pycache__/views.cpython-311.pyc 	modified:   Dentist/app/admin.py 	new file:   Dentist/app/migrations/0001_initial.py 	new file:   Dentist/app/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   Dentist/app/models.py 	new file:   Lib/site-packages/MySQLdb/__init__.py 	new file:   Lib/site-packages/MySQLdb/__pycache__/__init__.cpython-311.pyc 	new file:   Lib/site-packages/MySQLdb/__pycache__/_exceptions.cpython-311.pyc 	new file:   Lib/site-packages/MySQLdb/__pycache__/connections.cpython-311.pyc     	new file:   Lib/site-packages/MySQLdb/__pycache__/converters.cpython-311.pyc 	new file:   Lib/site-packages/MySQLdb/__pycache__/cursors.cpython-311.pyc 	new file:   Lib/site-packages/MySQLdb/__pycache__/release.cpython-311.pyc 	new file:   Lib/site-packages/MySQLdb/__pycache__/times.cpython-311.pyc 	new file:   Lib/site-packages/MySQLdb/_exceptions.py 	new file:   Lib/site-packages/MySQLdb/_mysql.cp311-win_amd64.pyd 	new file:   Lib/site-packages/MySQLdb/connections.py 	new file:   Lib/site-packages/MySQLdb/constants/CLIENT.py     	new file:   Lib/site-packages/MySQLdb/constants/CR.py 	new file:   Lib/site-packages/MySQLdb/constants/ER.py 	new file:   Lib/site-packages/MySQLdb/constants/FIELD_TYPE.py 	new file:   Lib/site-packages/MySQLdb/constants/FLAG.py 	new file:   Lib/site-packages/MySQLdb/constants/__init__.py 	new file:   Lib/site-packages/MySQLdb/constants/__pycache__/CLIENT.cpython-311.pyc 	new file:   Lib/site-packages/MySQLdb/constants/__pycache__/CR.cpython-311.pyc 	new file:   Lib/site-packages/MySQLdb/constants/__pycache__/ER.cpython-311.pyc 	new file:   Lib/site-packages/MySQLdb/constants/__pycache__/FIELD_TYPE.cpython-311.pyc 	new file:   Lib/site-packages/MySQLdb/constants/__pycache__/FLAG.cpython-311.pyc 	new file:   Lib/site-packages/MySQLdb/constants/__pycache__/__init__.cpython-311.pyc 	new file:   Lib/site-packages/MySQLdb/converters.py     	new file:   Lib/site-packages/MySQLdb/cursors.py 	new file:   Lib/site-packages/MySQLdb/release.py 	new file:   Lib/site-packages/MySQLdb/times.py 	new file:   Lib/site-packages/mysqlclient-2.1.1.dist-info/INSTALLER 	new file:   Lib/site-packages/mysqlclient-2.1.1.dist-info/LICENSE 	new file:   Lib/site-packages/mysqlclient-2.1.1.dist-info/METADATA 	new file:   Lib/site-packages/mysqlclient-2.1.1.dist-info/RECORD 	new file:   Lib/site-packages/mysqlclient-2.1.1.dist-info/REQUESTED 	new file:   Lib/site-packages/mysqlclient-2.1.1.dist-info/WHEEL     	new file:   Lib/site-packages/mysqlclient-2.1.1.dist-info/top_level.txt

--- Dentist/app/models.py
+++ Dentist/app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=16)
+    specialization = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+class Appointment(models.Model):
+    doctorname = models.CharField(max_length=50)
+    patientname = models.CharField(max_length=50)
+    patientemail = models.EmailField(max_length=50)
+    appointmentdate = models.DateField(max_length=10)
+    symptoms = models.CharField(max_length=100)
+    status = models.BooleanField()
+    
+    def __str__(self):
+        return self.patientname+"you have appointment with" +self.doctorname  
